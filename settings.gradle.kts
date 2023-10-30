bdcee09df94341121c2ae86d6085f6db9e4b3c23
tan-nguyen090102 <134823646+tan-nguyen090102@users.noreply.github.com>
2023-10-29 21:46:55 -0500

    Initial Application template for main

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Hearing Aid Application"
+include(":app")
+ 
