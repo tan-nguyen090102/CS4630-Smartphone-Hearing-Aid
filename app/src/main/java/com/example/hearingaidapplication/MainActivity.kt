1c501d9b181884a60f9e2c49dc2ed9aa8412cc66
tan-nguyen090102 <134823646+tan-nguyen090102@users.noreply.github.com>
2023-11-05 16:14:44 -0600

    MainActivity with simple UI to record a .wav file

--- app/src/main/java/com/example/hearingaidapplication/MainActivity.kt
+++ app/src/main/java/com/example/hearingaidapplication/MainActivity.kt
@@ -1,5 +1,4 @@
 package com.example.hearingaidapplication
-
 import android.graphics.Color
 import android.os.Bundle
 import android.widget.SeekBar
@@ -9,6 +8,20 @@ import com.masoudss.lib.SeekBarOnProgressChanged
 import java.io.File
 import com.masoudss.lib.WaveformSeekBar
 import com.masoudss.lib.utils.WaveGravity
+import android.Manifest
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import com.example.hearingaidapplication.ui.theme.HearingAidApplicationTheme
+import com.github.squti.androidwaverecorder.WaveRecorder
 
 class MainActivity : ComponentActivity() {
 
@@ -25,15 +38,15 @@ class MainActivity : ComponentActivity() {
             SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seek: SeekBar,
                                            progress: Int, fromUser: Boolean) {
-                // write custom code for progress is changed
+
             }
 
-            override fun onStartTrackingTouch(seek: SeekBar) {
-                // write custom code for progress is started
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+                TODO("Not yet implemented")
             }
 
-            override fun onStopTrackingTouch(seek: SeekBar) {
-                // write custom code for progress is stopped
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+                TODO("Not yet implemented")
             }
         })
         waveformSeekBar = findViewById(R.id.waveformSeekBar)
@@ -60,6 +73,47 @@ class MainActivity : ComponentActivity() {
 
         waveformSeekBar.setSampleFrom(R.raw.audio)
 
+        /**
+         * This path points to application cache directory.
+         * you could change it based on your usage
+         */
+        //Path: Internal Storage/Android/data/com.example.hearingaidapplication/cache
+        val filePath:String = externalCacheDir?.absolutePath + "/audio.wav"
+        Toast.makeText(this, filePath, Toast.LENGTH_LONG).show()
+        val waveRecorder = WaveRecorder(filePath)
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.RECORD_AUDIO),
+            0
+        )
+
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            0
+        )
+
+        setContent {
+            HearingAidApplicationTheme {
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick = {
+                        waveRecorder.startRecording()
+                    }) {
+                        Text(text = "Start recording")
+                    }
+                    Button(onClick = {
+                        waveRecorder.stopRecording()
+                    }) {
+                        Text(text = "Stop recording")
+                    }
+                }
+            }
+        }
     }
 }
 
