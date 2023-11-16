9b525aecb5fa8a1b9f6f0fa57aa1ae897943b0fc
Yasin Boloorchi <yasin.boloorchi@gmail.com>
2023-11-16 15:45:50 -0600

    Added network cpp file and its header to main/cpp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,11 @@ android {
         vectorDrawables {
             useSupportLibrary = true
         }
+        externalNativeBuild {
+            cmake {
+                cppFlags += ""
+            }
+        }
     }
 
     buildTypes {
@@ -45,6 +50,12 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path = file("src/main/cpp/CMakeLists.txt")
+            version = "3.22.1"
         }
     }
 }
